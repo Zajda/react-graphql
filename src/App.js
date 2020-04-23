0e3936b3d1db10f945583df34b2f89880be6ae63
Tom Zajda <zajt00@vse.cz>
2020-04-23 17:06:44 +0200

    Pokemon app with React and GraphQL  - request data from database  - container/component architecture  - using map to iterate through results and display them

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import ApolloClient from 'apollo-boost';
+import  {ApolloProvider} from '@apollo/react-hooks';
+import {PokemonContainer} from './containers/PokemonContainer';
+
+function App() {
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon.now.sh'
+  })
+
+  return (
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonContainer />
+      </main>
+    </ApolloProvider>
+  );
+}
+
+export default App;
